5e91d37df3cdb0a0fdb30ffa0177b0e6db88fc55
Elrickk <51099313+Elrickk@users.noreply.github.com>
2020-03-06 23:44:57 +0100

    Create Home_Elrick_Backup_Daily.sh

---
+++ Home_Elrick_Backup_Daily.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+rsync -a --progress /home/elrick/ /mnt/Backups/Home_elrick_daily/
+
+# -a  : permet de s’assurer que les liens symboliques, dates de modification, propriétaires et permissions sur les fichiers, sont conservés lors du transfert. 
+
+# --progress  : affiche la progression
