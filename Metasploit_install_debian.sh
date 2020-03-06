99428b3567f0e495c9c37546e54338989c632566
Elrickk <51099313+Elrickk@users.noreply.github.com>
2020-03-07 00:15:06 +0100

    Create Metasploit_install_debian.sh

---
+++ Metasploit_install_debian.sh
@@ -0,0 +1,23 @@
+# Download metasploit :
+
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+
+# Once the script is downloaded, make it executable :
+chmod +x msfinstall
+
+#Then execute the installer to install Metasploit Framework on Debian 10/9 :
+./msfinstall
+
+# Check your Framework version using :
+msfconsole --version
+
+# When installation is completed, create and initialize the msf database :
+msfdb init
+
+# This will create an initial database schema, set service account and start services. Output similar to below should be printed.
+
+# Now  that database is initialized, you can launch :
+msfconsole
+
+# Updating Metasploit framework :
+msfupdate
